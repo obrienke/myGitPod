c26038912c4490296f40203b1edb7f09a7d0baf3
obrienke <kevin.obrien@gmit.ie>
2019-09-23 08:55:35 +0000

    Basic types example

---
+++ Types.java
@@ -0,0 +1,32 @@
+public class Types{
+        public static void main(String[] args){
+            String message = "Thanks for the input!";
+            String name = "Kevin";
+            int age = 28;
+
+            System.out.println("Thank " + "you");
+
+            System.out.println(name+", "+message);
+
+            System.out.println(2 + 2);
+
+            System.out.println(age);
+
+            System.out.println("This is as really long piece"+
+                                " of text and this morning we "+
+                                "learned Darragh does not drink"+
+                                " coffee.");
+
+            //"Tom is 20. He got 47.5 in his maths exam"
+
+            name = "Tom";
+            age = 20;
+            double result = 47.5;
+            String subject = "maths";
+
+            System.out.println(name+" is "+age+". He got "+
+                                result+" in his "+subject+" exam");
+
+
+        }
+}
